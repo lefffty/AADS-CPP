4af50e9f2f45fe6b13c51d70885405ece8b634a8
leffty <brigante2004@yandex.ru>
2025-08-25 09:34:39 +0300

    LinkedList implementation

---
+++ lists/linkedlist.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+
+
+template <typename T>
+struct Node
+{
+    Node(T value){
+        this->value = value;
+    };
+
+    T value;
+    Node* next = nullptr;
+};
+
+template <typename T>
+class LinkedList
+{
+private:
+    Node<T>* head;
+    unsigned int size;
+public:
+    LinkedList(): head(nullptr), size(0) {}
+
+    unsigned int _size(){
+        return this->size;
+    }
+
+    void push(T value){
+        if (this->head == nullptr) {
+            head = new Node<T>(value);
+            this->size++;
+        }
+        else {
+            Node<T>* node = new Node<T>(value);
+            Node<T>* current = head;
+            while (current->next){
+                current = current->next;
+            }
+            current->next = node;
+            this->size++;
+        }
+    }
+
+    T pop(){
+
+    }
+
+    T get(){
+        return this->head->value;
+    }
+
+    bool find(T value){
+        auto current = this->head;
+        while (current) {
+            if (current.value == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void reverse(){
+        if (this->head){
+            Node<T>* current = nullptr;
+            while (head) {
+                Node<T>* temp = this->head->next;
+                this->head->next = current;
+                current = head;
+                head = temp;
+            }
+        }
+    }
+
+    bool is_empty(){
+        return this->size < 0;
+    }
+
+    void print(){
+        Node<T>* current = this->head;
+        while (current){
+            std::cout << current->value << " -> ";
+            current = current->next;
+        }
+        std::cout << "NULL";
+    }
+
+    ~LinkedList(){
+        Node<T>* current = this->head;
+        while (current) {
+            Node<T>* temp = current;
+            current = current->next;
+            delete temp;
+        }
+    }
+};
+
+
+int main(){
+    LinkedList<int>* list = new LinkedList<int>();
+    for (size_t i = 0; i < 10; i++)
+    {
+        list->push(i);
+    }
+    list->print();
+}
